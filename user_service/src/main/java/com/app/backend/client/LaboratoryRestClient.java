b631bdabd49aefe87fb2e9f7ae3be05efc50c7bc
mouna <mounaettalbi@gmail.com>
2024-11-11 00:56:33 +0100

    add default methods for circuit breaker

--- user_service/src/main/java/com/app/backend/client/LaboratoryRestClient.java
+++ user_service/src/main/java/com/app/backend/client/LaboratoryRestClient.java
@@ -7,13 +7,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.app.backend.model.Laboratory;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "laboratory-service")
 public interface LaboratoryRestClient {
 	
 	@GetMapping("/laboratory/{id}")
+	@CircuitBreaker(name="laboratoryService",fallbackMethod="getDefaultLaboratory")
 	Laboratory findLaboratoryById(@PathVariable Long id) ;
 	
 	@GetMapping("/laboratory/all")
+	@CircuitBreaker(name="laboratoryService",fallbackMethod="getDefaultAllLaboratory")
 	List<Laboratory> allLaboratory();
+	
+	default Laboratory getDefaultLaboratory(Long id, Exception exception) {
+		Laboratory labo =new Laboratory();
+		labo.setId(id);
+		labo.setNom("Not available");
+		labo.setNrc(null);
+		return labo;
+	}
+	
+	default List<Laboratory> getDefaultAllLaboratory(Exception exception){
+		return List.of();
+	}
 
 }
